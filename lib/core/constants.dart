fed5fdba4f9473f403f03b72d70019c1cb8d9be3
ss <mahmoudmagdy101019@gmail.com>
2023-12-09 00:04:20 +0200

    last commit

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -4,12 +4,6 @@ import 'package:flutter/material.dart';
 final scaffoldKey= GlobalKey<ScaffoldState>();
 final formKey= GlobalKey<FormState>();
 
-var nameController= TextEditingController();
-//var myCountry= CountryCode(name: "EG",dialCode: "+20");
-
-String? userId="";
-
-//apply job
 TextEditingController usernameController=TextEditingController();
 TextEditingController emailController=TextEditingController();
 TextEditingController phoneController=TextEditingController();
